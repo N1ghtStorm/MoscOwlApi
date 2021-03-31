f2a9a8a5cb65dd58c222e0d51bfb598a40894842
Kivenkantaja Kivenkantaja <bushuevdv@mail.ru>
2021-04-01 01:18:49 +0300

    add typeorm

--- src/Models/Models.ts
+++ src/Models/Models.ts
@@ -1,4 +1,23 @@
-export interface Player {
-    memberId: number,
-    Name: string,
+import {Entity} from "typeorm";
+
+@Entity()
+export class Player {
+    memberId: number;
+    fullName: string;
+    fourZeroCount: number;
 }
+
+@Entity()
+export class Flag {
+    flagId: number;
+    memberId: number;
+    flagName: string;
+}
+
+@Entity()
+export class User {
+    userName: string;
+    passwordHash: string;
+    passwordSalt: string;
+}
+
